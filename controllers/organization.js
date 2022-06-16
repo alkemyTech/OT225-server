b12dd8d9e5c548590a5f1ef92fe31a34cb7398e5
quirogatdf <quirogatdf@gmail.com>
2022-06-16 14:31:44 -0300

    add endpoint /organization/public

--- controllers/organization.js
+++ controllers/organization.js
@@ -5,7 +5,7 @@ class organizationControllers {
     static async getAll(req, res) {
         try{
             const organizations = await Organization.findAll({
-                attributes: ['name'],
+                attributes: ['name','image','phone','address'],
                 paranoid: false
             });
             res.status(200).json ({
