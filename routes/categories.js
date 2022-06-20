2a1c8e64293584baa3f07083da88852739ed2b7a
Fede Bramati <bramatif@hotmail.com>
2022-06-20 20:09:20 -0300

    Create and update Categories endpoints created

---
+++ routes/categories.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const { createCategory, updateCategory } = require('../controllers/categories/index.js');
+
+
+router.post('/', createCategory);
+router.put('/:id', updateCategory);
+
+module.exports = router;
