5930847af1bf654079c90229d7ba49f68bc7a8a4
Maximiliano Casanova <89485672+MaxiCassanova@users.noreply.github.com>
2022-06-21 13:03:08 -0300

    Merge categories controllers and routes for Update and Create

---
+++ controllers/categories/index.js
@@ -0,0 +1,33 @@
+const models = require('./../../models');
+
+const createCategory = async (req, res) =>{
+    try{
+        if(req.body.name !== null && typeof req.body.name === 'string'){
+            await models.Categories.create(req.body);
+            res.status(201).json({message: 'Category created'});
+        }else{
+            res.status(400).json({error: 'La categoría debe tener un nombre y ser un string'});
+        }
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+}
+
+const updateCategory = async (req, res) => {
+    try{
+        let data = await models.Categories.findOne({where: {id: req.params.id}});
+        if(data !== null){
+            await models.Categories.update(req.body, {where: {id: data.id}});
+            res.status(201).json({message: 'Categoría actualizada'});
+        }else{
+            res.status(400).json({error: 'La categoría solicitada no existe'});
+        }
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+}
+
+module.exports = {
+    createCategory,
+    updateCategory
+}; 
