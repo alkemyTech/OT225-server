351abf5bbdfccbc5821ab30838d1c2a36e43c3f2
1w1111789AimonettiJoaquin <Yaimonetti@hotmail.com>
2022-06-22 19:47:09 -0300

    creacion del endpoint crear novedad

--- controllers/news.js
+++ controllers/news.js
@@ -1,20 +1,41 @@
-const { News } = require ('../models/news')
-
+const { News } = require("../models/news");
+const models = require("../models");
 
 class newsController {
-    static async getAll(req, res) {
-        try{
-            const news = await News.findAll({
-
-            });
-            res.status(200).json ({
-            
-            })
-
-        } catch (error) {
-            return res.status(500).json ({ error })
-        }
+  static async getAll(req, res) {
+    try {
+      const news = await News.findAll({});
+      res.status(200).json({});
+    } catch (error) {
+      return res.status(500).json({ error });
     }
+  }
 }
 
-module.exports = newsController;
+const createNew = async (req, res) => {
+  try {
+    if (
+      req.body.name !== "" &&
+      typeof req.body.name === "string" &&
+      req.body.content !== "" &&
+      typeof req.body.name === "string" &&
+      req.body.image !== "" &&
+      typeof req.body.image === "string" && 
+      !isNaN(req.body.categoryId) 
+    ) {
+      await models.News.create(req.body);
+      res.status(201).json({ message: "New created" });
+    } else {
+      res
+        .status(400)
+        .json({ err: "Datos ingresados incorrectos." });
+    }
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+};
+
+module.exports = {
+  newsController,
+  createNew,
+};
--- models/news.js
+++ models/news.js
@@ -9,9 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    /* static associate(models) {
       News.belongsTo(models.Category, {as: 'category'});
-    }
+    } */
   };
   News.init({
     name: DataTypes.STRING,
