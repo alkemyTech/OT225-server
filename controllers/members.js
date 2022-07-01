5d0dbc87efc99108589682b071100a061d09f8cc
quirogatdf <quirogatdf@gmail.com>
2022-06-30 23:52:16 -0300

    ot225-74/Add a new Member

--- controllers/members.js
+++ controllers/members.js
@@ -1,5 +1,6 @@
 const { Members } = require ('../models')
 class membersControllers {
+    /* List all Members */
     static async getAll(req, res) {
         try{
             const members = await Members.findAll({
@@ -12,6 +13,22 @@ class membersControllers {
         } catch (error) {
             return res.status(500).json ({ error: error.message})
         }
+    };
+    /* Create new member */
+    static async add(req, res) {
+        const {name, image} = req.body
+        try{
+            if((name) && (typeof(name)==='string')){
+                await Members.create(req.body)
+                return res.status(200).json({ success: true, message: `El miembro se ha creado con éxito` });
+            } else {
+                return res.status(400).json({ success: false, message: `Verifique que el campo nombre no sea Null, y que sea del tipo STRING` });
+            }
+
+        }catch (error) {
+            return res.status(500).json({error: error.message})
+        }
+
     }
 }
 
