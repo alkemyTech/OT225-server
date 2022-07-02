7341eca321985f31072b568d2c8fd345e285cec4
Estanip <estani.pettigrew@gmail.com>
2022-07-02 20:44:10 -0300

    add create, update, getall, getdetail, delete slides controller

--- controllers/slides.js
+++ controllers/slides.js
@@ -27,7 +27,7 @@ const createSlide = async (req, res) => {
             const image = await imageUpload(imageUrl)
             imageUrl = image
 
-            // Check if we are receiving order, if not create at first, or the highest
+            // Check if we are receiving order, if not create at first, or next to highest
             if (!order) {
                 // look for the highest value for order
                 const { maxOrder } = await Slides.findOne({
@@ -54,8 +54,123 @@ const createSlide = async (req, res) => {
                 order
             }
 
-            const slide = await Slides.create(newSlide);
-            return res.json(slide)
+            await Slides.create(newSlide);
+            return res.status(201).json("Slide creado con exito")
+
+        } else {
+            return res.status(400).json("No se han ingresado los datos correctamente");
+        }
+
+    } catch (error) {
+        return res.status(500).json({
+            Error: error.message
+        })
+    }
+};
+
+/**
+*  Get all Slides
+* @function
+* @returns {Promise<Slide[]|Error>} - Array with Slide Object or Error
+*/
+const getSlides = async (req, res) => {
+    try {
+        const slides = await Slides.findAll({
+            attributes: ['imageUrl', 'text', 'order'],
+            paranoid: false,
+        });
+
+        if (slides.length === 0) {
+            return res.status(404).json("No hay Slides creados")
+        }
+
+        return res.status(201).json(slides)
+
+    } catch (error) {
+        return res.status(500).json({
+            Error: error.message
+        })
+    }
+};
+
+/**
+* Get detail of a Slide (find by id) 
+* @function
+* @param {express.Request} req.params.id - Slide ID
+* @returns {Promise<Slide|Error>} - Slide Object or Error
+*/
+const getSlideDetails = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const slide = await Slides.findOne({
+            where: { id: id }
+        });
+        if (!slide) {
+            return res.status(404).json("El slide solicitado no existe")
+        }
+        return res.status(200).json(slide)
+    } catch (error) {
+        return res.status(500).json({
+            Error: error.message
+        })
+    }
+};
+
+/**
+ * Udpdate a Slide (find by id) 
+ * @function
+ * @param {express.Request} Slide - Slide Object
+ * @param {express.Request} req.params.id - Slide ID 
+ * @returns {Promise<String|Error>} - Message Response or Error 
+ */
+const updateSlide = async (req, res) => {
+    const { id } = req.params;
+    let { imageUrl, text, order } = req.body;
+    try {
+        const slide = await Slides.findOne({ where: { id: id } });
+        if (!slide) {
+            return res.status(404).json("El slide a actualizar no existe")
+        } else {
+            // check if we arereceiving imageUlr and text
+            if (imageUrl && text) {
+                const image = await imageUpload(imageUrl)
+                imageUrl = image
+
+                // Check if we are receiving order, if not create at first, or next to highest
+                if (!order) {
+                    // look for the highest value for order
+                    const { maxOrder } = await Slides.findOne({
+                        attributes: [[
+                            sequelize.fn('max', sequelize.col('order')), 'maxOrder'
+                        ]],
+                        paranoid: false,
+                        raw: true
+                    });
+
+                    // if not exist any object in DB, assign number 1
+                    if (!maxOrder) {
+                        order = 1
+                    }
+                    else {
+                        order = maxOrder + 1;
+                    }
+                }
+
+                // slide object to create
+                let newSlide = {
+                    imageUrl,
+                    text,
+                    order
+                }
+
+                await Slides.update(newSlide, { where: { id: slide.id } });
+                return res.status(201).json("Slide actualizado con exito")
+
+            } else {
+                return res.status(400).json("No se han ingresado los datos correctamente");
+            }
+
         }
     } catch (error) {
         return res.status(500).json({
@@ -64,4 +179,27 @@ const createSlide = async (req, res) => {
     }
 }
 
-module.exports = { createSlide };
+/**
+ * Delete a Slide (find by id)
+ * @function
+ * @param {express.Request} req.params.id - Slide ID 
+ * @returns {Promise<String|Error>} - Message Response or Error 
+ */
+const deleteSlide = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const slide = await Slides.findOne({ where: { id: id } })
+        if (!slide) {
+            res.status(400).json("El slide a eliminar no existe")
+        } else {
+            await Slides.destroy({ where: { id: slide.id } })
+            res.status(200).json("Slide eliminado con exito")
+        }
+    } catch (error) {
+        res.status(500).json({
+            Error: error.message
+        })
+    }
+};
+
+module.exports = { createSlide, getSlides, getSlideDetails, updateSlide, deleteSlide };
--- routes/slides.js
+++ routes/slides.js
@@ -1,9 +1,17 @@
 const { Router } = require("express");
 const router = Router();
 
-const { createSlide } = require("../controllers/slides");
+const { createSlide, getSlides, getSlideDetails, updateSlide, deleteSlide} = require("../controllers/slides");
 
+/** Get all Slides */
+router.get('/', getSlides);
+/** Get detail of a Slide */
+router.get('/:id', getSlideDetails);
 /** Create a Slide */
-router.post("/", createSlide);
+router.post('/', createSlide);
+/** Update a Slide */
+router.put('/:id', updateSlide);
+/** Delete a Slide */
+router.delete('/:id', deleteSlide);
 
 module.exports = router;
