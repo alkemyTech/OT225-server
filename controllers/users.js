ae6d25a96f209e8fbe6139799648f8adf579e2fe
Fede Bramati <bramatif@hotmail.com>
2022-07-14 12:25:40 -0300

    list, update, delete Users endpoints added

--- controllers/users.js
+++ controllers/users.js
@@ -142,4 +142,69 @@ const authenticatedUser = async (req, res) => {
     }
 };
 
-module.exports = { register, login, authenticatedUser };
+
+//Permite listar usuarios almacenados en base de datos
+const listUsers = async (req, res) => {
+    try{
+        let data = await User.findAll({
+            attributes: ['firstName', 'lastName', 'email', 'image']
+        });
+        if(data.lenght !== 0){
+            res.status(200).json({data: data});
+        }else{
+            res.status(400).json({error: 'No hay usuarios para mostar'});
+        };
+    }catch(error){
+        res.status(500).json({error: error.message});
+    };
+};
+
+//Permite actualizar los datos de un usuario
+const updateUser = async (req, res) => {
+    try{
+        let user = await User.findOne({
+            where: {id: req.params.id}
+        });
+        if(user !== null){
+            await User.update(req.body, {where: {id: user.id}});
+            res.status(200).json({message: 'Usuario actualizado'});
+        }else{
+            res.status(404).json({error: 'Usuario no encontrado'});
+        };
+    }catch(error){
+        res.status(500).json({error: error.message});
+    };
+};
+
+//Permite realizar un borrado logico de un usuario
+const deleteUser = async (req, res) => {
+    try{
+        let user = await User.findOne({
+            where: {id: req.params.id}
+        });
+        if(user !== null){
+            await User.destroy({where: {id: user.id}});
+            res.status(200).json({message: 'Usuario eliminado'});
+        }else{
+            res.status(404).json({error: 'El usuario no existe'});
+        };
+    }catch(error){
+        res.status(500).json({error: error.message});
+    };
+};
+
+
+
+
+
+
+
+
+module.exports = { 
+    register, 
+    login, 
+    authenticatedUser,
+    listUsers, 
+    updateUser,
+    deleteUser
+};
