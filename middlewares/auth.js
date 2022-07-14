9d79070fdd632f7df1af96e2fa9519ea9aee39fd
Fede Bramati <bramatif@hotmail.com>
2022-07-14 14:09:31 -0300

    Auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+require("dotenv").config();
 
 
 async function verifyRole(req, res, next) {
@@ -15,4 +16,20 @@ async function verifyRole(req, res, next) {
         res.status(401).json({error:'Unauthorized', message: 'Token verification failed'})
     }
 }
-module.exports = {verifyRole}
+
+const verifyToken = (req, res, next) => {
+    try{
+        const token = req.header('Authorization')
+        if(!token){
+            res.status(403).json({error: 'Unauthorized', message: ' Access denied'});            
+        }else{
+            const decoded = jwt.verify(token, process.env.SECRET_JWT);
+            req.user = decoded;
+            next();
+        }
+    }catch(error){
+        res.status(401).json({error: 'Invalid token', message: error.message});
+    };
+};
+
+module.exports = {verifyRole, verifyToken}
